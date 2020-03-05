0fd5a90116cc7c3bbc13019b7d586d58d09e2c3c
Nazar Kaluzghnyy <nazar.kaliuzghnyy@uadevelopers.com>
2020-03-05 20:36:36 +0200

    Get fulfil api endpoint from env variable

--- chalicelib/__init__.py
+++ chalicelib/__init__.py
@@ -1,4 +1,6 @@
+import os
+
 RUBYHAS_HQ_STORAGE = 26
 AURATE_HQ_STORAGE = 3
 COMPANY = 1
-FULFIL_API_URL = 'https://aurate-sandbox.fulfil.io/api/v2'
+FULFIL_API_URL = os.environ.get('FULFIL_API_ENDPOINT')
