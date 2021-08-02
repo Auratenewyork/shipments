a46629fd7ed73ab80f1316cb649823088a74c74f
fibio <fibio.tany@gmail.com>
2021-08-02 19:13:29 +0300

    Changed TmallOrderConverter

--- chalicelib/tmall_utils.py
+++ chalicelib/tmall_utils.py
@@ -8,20 +8,28 @@ class TmallOrderConverter:
 
     def make_fake_email(self, address):
         base_val = '{} {} {} {}'.format(
-            address['name'],
-            address['address1'],
-            address['phone'],
-            address['city']
+            address.get('name'),
+            address.get('address1'),
+            address.get('phone'),
+            address.get('city')
         )
         return 'email{}@qq.com'.format(hash(base_val))
 
     def validate_email(self):
+        faked_email = None
         for address_type in ('billing_address', 'shipping_address'):
             address = self.order.get(address_type)
             if address:
                 email = address.get('email')
-                if email in self.default_emails or 'fake' in email:
+                if email in self.default_emails:
+                    faked_email = self.make_fake_email(address)
                     address['email'] = self.make_fake_email(address)
+        customer = self.order['customer']
+        contact = customer['contacts'][0]
+        email = contact[0]
+        if email in self.default_emails:
+            faked_email = faked_email or self.make_fake_email(contact)
+            contact.insert(1, faked_email)
 
     def get_order(self):
         self.validate_email()
